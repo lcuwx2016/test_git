ce0701b4c2329e91df4035c2aa6a0bff42645592
Wang Xiang <31233561+lcuwx2016@users.noreply.github.com>
2018-09-02 10:14:33 +0800

    first_commit

---
+++ src/cn/wx2020/testservlet/HelloWorldServlet.java
@@ -0,0 +1,13 @@
+package cn.wx2020.testservlet;
+
+import java.io.*;
+import javax.servlet.*;
+
+public class HelloWorldServlet  extends GenericServlet {
+	public void service(ServletRequest request, ServletResponse response) 
+			throws ServletException, IOException{
+		PrintWriter out = response.getWriter();
+		out.println("Hello World");	
+	}
+
+}
